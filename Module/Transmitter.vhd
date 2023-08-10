2846789301ee521b7af5d5f0e78d091397d9c5ab
Mohammad Mahdi Nemati Haravani <nemati.mahdi255@gmail.com>
2023-08-10 17:29:15 +0330

    UART Transmiter Update

--- Module/Transmitter.vhd
+++ Module/Transmitter.vhd
@@ -14,7 +14,7 @@ entity Transmitter is
 		i_EN        : in  std_logic;
 		i_TX_CLK    : in  std_logic;
 		i_TX_DATA   : in  std_logic_vector (WIDTH - 1 downto 0);
-		i_TX_STR     : in  std_logic;
+		i_TX_STR    : in  std_logic;
 		i_PARITY_EN : in  std_logic;
 		o_TX_RDY    : out std_logic;
 		o_TX_SDO    : out std_logic
@@ -23,62 +23,100 @@ end Transmitter;
 
 architecture RTL of Transmitter is
 
-	type t_FSM is (START_BIT, DATA_BITS, PARITY_BIT, STOP_BIT);
+	type t_FSM is (IDLE, START_BIT, DATA_BITS, PARITY_BIT, STOP_BIT);
 
-	signal r_PR_ST           : t_FSM                                := START_BIT;
-	signal r_TX_DATA         : std_logic_vector(WIDTH - 1 downto 0) := (others => '0');
-	signal r_PARITY_BIT      : std_logic                            := '0';
-	signal r_BIT_COUNT       : integer range 0 to WIDTH - 1         := 0;
+	signal r_PR_ST      : t_FSM                                := IDLE;
+	signal r_NX_ST      : t_FSM                                := IDLE;
+	signal r_TX_DATA    : std_logic_vector(WIDTH - 1 downto 0) := (others => '0');
+	signal r_PARITY_BIT : std_logic                            := '0';
+	signal r_BIT_COUNT  : integer range 0 to WIDTH - 1         := 0;
 
 begin
 
-	Transmitter : process (i_EN, i_TX_CLK)
+	Sequential : process (i_EN, i_TX_CLK)
 	begin
+
 		if i_EN = '0' then
-			r_PR_ST      <= START_BIT;
+			r_PR_ST <= IDLE;
+		elsif rising_edge(i_TX_CLK) then
+			r_PR_ST <= r_NX_ST;
+		end if;
+
+		if i_EN = '0' then
 			r_TX_DATA    <= (others => '0');
-			r_PARITY_BIT <= '0';
 			r_BIT_COUNT  <= 0;
-			o_TX_RDY     <= '1';
-			o_TX_SDO     <= '1';
+			r_PARITY_BIT <= '0';
 
 		elsif rising_edge(i_TX_CLK) then
 			case r_PR_ST is
-				when START_BIT =>
-					o_TX_RDY <= not i_TX_STR;
-					o_TX_SDO <= not i_TX_STR;
-
+				when IDLE | STOP_BIT =>
 					if i_TX_STR = '1' then
-						r_TX_DATA    <= i_TX_DATA;
-						r_PARITY_BIT <= calculate_parity_bit(i_TX_DATA);
-						r_PR_ST      <= DATA_BITS;
+						r_TX_DATA   <= i_TX_DATA;
 					end if;
+					
+				when START_BIT =>
+					r_PARITY_BIT <= calculate_parity_bit(r_TX_DATA);
 
 				when DATA_BITS =>
-					o_TX_SDO    <= r_TX_DATA(r_TX_DATA'left);
 					r_TX_DATA   <= r_TX_DATA(r_TX_DATA'left - 1 downto 0) & '0';
 					r_BIT_COUNT <= r_BIT_COUNT + 1;
-					if r_BIT_COUNT = WIDTH - 1 then
-						r_BIT_COUNT <= 0;
+					
+				when PARITY_BIT =>	
+					r_BIT_COUNT <= 0;
+			end case;
+		end if;
 
+	end process Sequential;
+
+	combinational : process (i_EN, r_PR_ST, i_TX_STR, r_TX_DATA, r_BIT_COUNT, i_PARITY_EN, r_PARITY_BIT)
+	begin
+		if i_EN = '0' then
+			r_NX_ST  <= IDLE;
+			o_TX_SDO <= '1';
+
+		else
+			r_NX_ST <= r_PR_ST;
+
+			case r_PR_ST is
+				when IDLE =>
+					o_TX_SDO <= '1';
+					if i_TX_STR = '1' then
+						r_NX_ST <= START_BIT;
+					end if;
+
+				when START_BIT =>
+					o_TX_SDO <= '0';
+					r_NX_ST  <= DATA_BITS;
+
+				when DATA_BITS =>
+					o_TX_SDO <= r_TX_DATA(r_TX_DATA'left);
+
+					if r_BIT_COUNT = WIDTH - 1 then
 						if i_PARITY_EN = '1' then
-							r_PR_ST <= PARITY_BIT;
+							r_NX_ST <= PARITY_BIT;
 						else
-							r_PR_ST <= STOP_BIT;
+							r_NX_ST <= STOP_BIT;
 						end if;
 					end if;
 
 				when PARITY_BIT =>
 					o_TX_SDO <= r_PARITY_BIT;
-					r_PR_ST  <= STOP_BIT;
+					r_NX_ST  <= STOP_BIT;
 
 				when STOP_BIT =>
 					o_TX_SDO <= '1';
-					r_PR_ST  <= START_BIT;
+					if i_TX_STR = '1' then
+						r_NX_ST <= START_BIT;
+					else
+						r_NX_ST <= IDLE;
+					end if;
 
 			end case;
 
 		end if;
-	end process Transmitter;
+	end process combinational;
+
+	o_TX_RDY <= '1' when r_PR_ST = IDLE or r_PR_ST = STOP_BIT else
+		'0';
 
 end RTL;
